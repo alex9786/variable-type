ed3665af30e499692c6de9e0d8a594e6b4e72d76
alex9786 <alexdce007@gmail.com>
2024-03-14 12:23:23 +0530

    first commit

---
+++ variabletype.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package world;
+
+/**
+ * 
+ */
+public class variabletype {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a= 45;
+		String name ="ALEX";
+		double height =5.6;
+		boolean recived = false;
+		
+		
+		System.out.println(a);
+		System.out.println(name);
+		System.out.println(height);
+		System.out.println(recived);
+		
+		
+		
+		
+	}
+
+}
